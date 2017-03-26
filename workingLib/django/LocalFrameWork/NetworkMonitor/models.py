f577db3bcc1ade574cb81704f4f7a7fecb5d5ae3
davyeivi@gmail.com <davyeivi@gmail.com>
2017-03-26 19:29:33 +0300

    second commit

---
+++ workingLib/django/LocalFrameWork/NetworkMonitor/models.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+from Monitor import *
+from django.http import HttpResponse
+import json
+from django.db import models
+
---
+++ workingLib/django/LocalFrameWork/NetworkMonitor/urls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
