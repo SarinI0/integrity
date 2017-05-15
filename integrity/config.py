a4cb55ec4ec3132c1311e9da9b25eff5cdc03aea
Sarin.io <relay@sarin.io>
2017-05-16 01:03:58 +0300

    Update config.py

---
+++ integrity/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+
+pt = os.getcwd()
+pa = ""
+pa += pt + "/singleThread/utils/"
+execv = "sudo python " + pa + "insert.py"
+os.system(execv)
+os.chdir(pa)
+os.system("sudo rm insert.py")
+os.chdir(pt)
