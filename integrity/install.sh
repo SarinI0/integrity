815b7bce53e6d923b8ea6e8c50e0f09d385174c3
Sarin.io <relay@sarin.io>
2017-05-16 00:17:15 +0300

    Update install.sh

--- integrity/install.sh
+++ integrity/install.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
 
-sudo pip install 
+sudo pip install pysqlite -y
+sudo pip install selenium -y
+sudo pip install pandas -y
+sudo pip install requests -y
+sudo apt-cache search beautifulsoup
+sudo apt-get install python2-bs4 -y
+sudo pip install html2text
+
+
+
+
