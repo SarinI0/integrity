97dc342a5f87a343bfe2aa76731c7c02ccb9d337
Sarin.io <relay@sarin.io>
2017-05-16 00:56:31 +0300

    Update install.sh

--- integrity/install.sh
+++ integrity/install.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ $(whoami) != 'root' ]; then 
+	echo "Must be root to run $0";
+	echo "run as sudo"
+	exit
 fi
-
-sudo pip install pysqlite -y
-sudo pip install selenium -y
-sudo pip install requests -y
-sudo apt-cache search beautifulsoup
-sudo apt-get install python2-bs4 -y
-sudo pip install html2text -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install build-essential ruby-dev libpcap-dev -y
-sudo gem install rubysl-open3 -y
-sudo gem install twilio-ruby -y
-sudo gem install shellex -y
+function pyInstall(){
+	sudo pip install $1 -y
+}
+function Install(){
+	sudo gem install $1 -y
+}
+pack=(rubysl-open3 twilio-ruby shellex)
+dep=(pysqlite selenium requests html2text )
+for i in "${pack[@]}"
+do
+   Install $i
+done
+for i in "${dep[@]}"
+do
+   pyInstall $i
+done
+
+sudo apt-cache search beautifulsoup
+sudo apt-get install  -y
 sudo python config.py
 
 
