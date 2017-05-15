566d809a28a559e4466da9d57386f5b7f84a9dca
Sarin.io <relay@sarin.io>
2017-05-16 00:45:41 +0300

    Update install.sh

--- integrity/install.sh
+++ integrity/install.sh
@@ -2,11 +2,17 @@
 
 sudo pip install pysqlite -y
 sudo pip install selenium -y
-sudo pip install pandas -y
 sudo pip install requests -y
 sudo apt-cache search beautifulsoup
 sudo apt-get install python2-bs4 -y
-sudo pip install html2text
+sudo pip install html2text -y
+sudo apt-get install build-essential ruby-dev libpcap-dev -y
+sudo gem install rubysl-open3 -y
+sudo gem install twilio-ruby -y
+sudo gem install shellex -y
+
+
+
 
 
 
