ead5ec788d33c8f3bde0be462c197db511732bbb
Sarin.io <relay@sarin.io>
2017-05-16 01:26:29 +0300

    Update config.py

--- integrity/config.py
+++ integrity/config.py
@@ -9,6 +9,7 @@ execv = "sudo python " + pa + "insert.py"
 os.system(execv)
 os.chdir(pa)
 os.system("sudo rm insert.py")
-os.chdir(pt)
+os.chdir(pt + "/singleThread")
+os.system("sudo mkdir log")
 
 
